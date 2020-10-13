b07759ab7f9dd9a3cd40fab84d4475afe8f332b6
Hendrik Schreiber <hs@tagtraum.com>
2020-10-13 12:25:26 +0200

    Improve classifier test coverage.

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -1,9 +1,10 @@
 import os
+from unittest.mock import patch
 
 import numpy as np
 import pytest
 
-from tempocnn.classifier import TempoClassifier
+from tempocnn.classifier import TempoClassifier, MeterClassifier, _load_model_from_github
 from tempocnn.feature import read_features
 
 
@@ -23,14 +24,32 @@ def test_track():
     return os.path.join(dir, 'data', 'drumtrack.mp3')
 
 
-def test_init():
-    tempo_classifier = TempoClassifier('fcn')
+@pytest.mark.parametrize('model_name', [
+    'fcn',
+    'cnn',
+    'ismir2018',
+    'fcn',
+    'mazurka',
+    'deeptemp',
+    'shallowtemp',
+    'deepsquare'
+])
+@patch("tempocnn.classifier.load_model")
+def test_init(mock_load_model, model_name):
+    mock_load_model.return_value = 'model'
+    tempo_classifier = TempoClassifier(model_name)
     assert tempo_classifier.model is not None
+    mock_load_model.assert_called_once()
 
 
-def test_bad_model_name():
+def test_bad_tempo_model_name():
     with pytest.raises(FileNotFoundError):
         TempoClassifier('bad_model_name')
+
+
+def test_bad_meter_model_name():
+    with pytest.raises(FileNotFoundError):
+        MeterClassifier('bad_model_name')
 
 
 def test_predict(test_data):
@@ -76,8 +95,41 @@ def test_mirex_with_real_data(test_track):
     assert t2 == pytest.approx(201.)
 
 
+def test_mirex_with_real_data_interpolate(test_track):
+    features = read_features(test_track)
+    tempo_classifier = TempoClassifier('fcn')
+    t1, t2, s1 = tempo_classifier.estimate_mirex(features, interpolate=True)
+    assert s1 == pytest.approx(0.99617153, abs=0.01)
+    assert t1 == pytest.approx(100.000, abs=0.001)
+    assert t2 == pytest.approx(200.984, abs=0.001)
+
+
 def test_tempo_with_real_data(test_track):
     features = read_features(test_track)
     tempo_classifier = TempoClassifier('fcn')
     tempo = tempo_classifier.estimate_tempo(features)
     assert tempo == pytest.approx(100.)
+
+
+def test_quad_interpol_argmax():
+    index, strength = TempoClassifier.quad_interpol_argmax(np.array([0, 2, 0]))
+    assert index == pytest.approx(1.)
+    assert strength == pytest.approx(2.)
+
+
+def test_quad_interpol_argmax_x_zero():
+    index, strength = TempoClassifier.quad_interpol_argmax(np.array([0, 2, 0]), x=0)
+    assert index == pytest.approx(0.)
+    assert strength == pytest.approx(0.)
+
+
+def test_quad_interpol_argmax_x_non_zero():
+    index, strength = TempoClassifier.quad_interpol_argmax(np.array([0, 2, 0]), x=1)
+    assert index == pytest.approx(1.)
+    assert strength == pytest.approx(2.)
+
+
+def test_load_model_from_github():
+    # smallest model
+    file = _load_model_from_github('models/deepsquare_k1.h5')
+    assert file is not None
